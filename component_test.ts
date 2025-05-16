85bd73e6d5b610ce49d74ca9ee1f7f352e3a1bdc
TrueRyoB <121699111+TrueRyoB@users.noreply.github.com>
2025-05-16 03:37:10 -0500

    Create component_test.ts

---
+++ component_test.ts
@@ -0,0 +1,13 @@
+describe('TitleComponent', () => {
+    it('should render application title as <h1>', async () => {
+      // テストアプリケーションのセットアップ
+      await TestBed.configureTestingModule({
+        imports: [TitleComponent],
+      }).compileComponents();
+      // コンポーネントインスタンスの生成
+      const fixture = TestBed.createComponent(TitleComponent);
+      const element = fixture.nativeElement as HTMLElement;
+      // DOMのアサーション
+      expect(element.querySelector('h1')?.textContent).toContain('My Applciation');
+    });
+});
